bdc0748dc4ceb74a7609831350e10219c9f8b8e5
Josh Pitman <shidogrey@gmail.com>
2016-01-31 16:56:50 -0500

    closer

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require('express'),
     server = express();
 
+server.use(express.static(__dirname+'/css'));
+
 server.get('/', function(req, res){
 	res.sendFile('html/index.html', { root: __dirname });
+});
+
+server.post('/signup', function(req, res){
+  console.log('you have signed away your first born');
+  res.redirect('/');
 });
 
 server.listen(8080, function(){
